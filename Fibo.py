e51e2ebd2d1d4a3b25070dbf106a156adf58ae02
Hossam Alghossini <h.alghossini@gmail.com>
2022-07-19 14:51:26 +0200

    adding Fibo and Function files

---
+++ Fibo.py
@@ -0,0 +1,20 @@
+"""
+Ecrire l'algorithme de la suite de fibonacci:
+C'est une suite définie par Un = Un-1 + Un-2
+Si vous trouvez ça trop facile, passez par une fonction récursive
+( c'est à dire fonction qui s'appelle elle meme)
+"""
+
+import Fonction
+
+def Fibonacci(nombre):
+    elem_fibonacci=[0,1]
+    for i in range(2,nombre):
+        elem_fibonacci.insert(i,elem_fibonacci[i-1]+elem_fibonacci[i-2])
+    print(elem_fibonacci)
+
+nombreSerie=0
+while nombreSerie<=1:
+    nombreSerie=Fonction.input_int_positif("Entrer un entier positif plus que 1 correspondant à nombre d'elements souhaités de la serie Fibonacci")
+
+Fibonacci(nombreSerie)
