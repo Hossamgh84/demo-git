30c11ffa28abd9debbe8d269bb0628d63ecd8b4b
Hossam Alghossini <h.alghossini@gmail.com>
2022-07-19 14:57:17 +0200

    modif fibo

--- Fibo.py
+++ Fibo.py
@@ -5,9 +5,9 @@ Si vous trouvez ça trop facile, passez par une fonction récursive
 ( c'est à dire fonction qui s'appelle elle meme)
 """
 
-import Fonction
+import fonction
 
-def Fibonacci(nombre):
+def fibo(nombre):
     elem_fibonacci=[0,1]
     for i in range(2,nombre):
         elem_fibonacci.insert(i,elem_fibonacci[i-1]+elem_fibonacci[i-2])
@@ -15,6 +15,7 @@ def Fibonacci(nombre):
 
 nombreSerie=0
 while nombreSerie<=1:
-    nombreSerie=Fonction.input_int_positif("Entrer un entier positif plus que 1 correspondant à nombre d'elements souhaités de la serie Fibonacci")
+    msg="Entrer un entier positif plus que 1 correspondant à nombre d'elements souhaités de la serie Fibonacci"
+    nombreSerie=fonction.input_int_positif(msg)
 
-Fibonacci(nombreSerie)
+fibo(nombreSerie)
